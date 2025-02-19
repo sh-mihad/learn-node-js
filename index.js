16c4d908210249c4267ad57b4219a1043f27bbcd
Sabbir <sh.mihad.cse@gmail.com>
2025-02-19 15:53:44 +0600

    raw node js server

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-const os = require('os');
+const http = require('node:http');
+const portNumber = process.env.PORT_NUMBER
 
-console.log("os Result",os.cpus());
+const server = http.createServer((req,res)=>{
+    res.setHeader("Content-Type"," text/html")
+    res.end("<h1>hello mello</h1>")
+})
+
+server.listen(portNumber,()=>{
+    console.log("server is running port",portNumber);
+})
+
+
