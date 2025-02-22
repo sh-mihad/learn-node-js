9f17e4d791b2c325f064a87c9f60b0296cf42b0f
Sabbir <sh.mihad.cse@gmail.com>
2025-02-22 22:49:10 +0600

    crud oparation done with mongose

--- controller/productController.js
+++ controller/productController.js
@@ -1,65 +1,78 @@
-const model =require("../model/product")
-const Product = model.Product
+const model = require("../model/product");
+const Product = model.Product;
 
 // apis
 
 // product create
-const createProducts =async (req, res) => {
- const product =  new Product(req.body)
-try {
-  const result = await product.save()
-  res.status(201).json(result)
-} catch (error) {
-  res.status(400).json(error.message)
-}  
+const createProducts = async (req, res) => {
+  const product = new Product(req.body);
+  try {
+    const result = await product.save();
+    res.status(201).json(result);
+  } catch (error) {
+    res.status(400).json(error.message);
+  }
 };
 
 // get all products
-const getProducts =async (req, res) => {
+const getProducts = async (req, res) => {
   try {
-    const products = await Product.find()
-    res.status(200).json(products)
+    const products = await Product.find();
+    res.status(200).json(products);
   } catch (error) {
-    res.status(404).json(error.message)
+    res.status(404).json(error.message);
   }
-  
 };
 
 // get a product by ID
-const getProduct = async(req, res) => {
+const getProduct = async (req, res) => {
   try {
-    const id = req.params.id
-    const product = await Product.findById(id)
-    res.status(200).json(product)
+    const id = req.params.id;
+    const product = await Product.findById(id);
+    res.status(200).json(product);
   } catch (error) {
-    res.status(404).json(error.message)
+    res.status(404).json(error.message);
   }
 };
 
-// replace product by ID
-const modifiedProduct = (req, res) => {
-  const id = +req.params.id;
-  const productIndex = products.findIndex((p) => p.id === id);
-  products.splice(productIndex, 1, { ...req.body, id: id });
-  res.status(201).json();
+// replace product by ID -- put method
+const modifiedProduct = async (req, res) => {
+  const id = req.params.id;
+  try {
+    const product = await Product.findOneAndReplace({ _id: id }, req.body, {
+      returnDocument: "after",
+    });
+
+    res.json(product);
+  } catch (error) {
+    res.status(404).json(error);
+  }
 };
 
 // update product by ID
-const updatedProduct = (req, res) => {
-  const id = +req.params.id;
-  const productIndex = products.findIndex((p) => p.id === id);
-  const product = products[productIndex];
-  products.splice(productIndex, 1, { ...product, ...req.body });
-  res.status(201).json();
+const updatedProduct = async (req, res) => {
+  const id = req.params.id;
+  try {
+    const updatedProduct = await Product.findOneAndUpdate(
+      { _id: id },
+      req.body,
+      { upsert: true, new: true }
+    );
+    res.json(updatedProduct);
+  } catch (error) {
+    res.status(404).json(error);
+  }
 };
 
 // delete product by ID
-const deleteProduct = (req, res) => {
-  const id = +req.params.id;
-  const productIndex = products.findIndex((p) => p.id === id);
-  const product = products[productIndex];
-  products.splice(productIndex, 1);
-  res.status(201).json(product);
+const deleteProduct = async (req, res) => {
+  const id = req.params.id;
+try {
+  const result = await Product.findOneAndDelete(id)
+  res.send(result)
+} catch (error) {
+  res.status(404).json(error);
+}
 };
 
 module.exports = {
